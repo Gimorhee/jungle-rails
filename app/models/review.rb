f68ae70ccbcbcec0fee2085f71b9f0ecaec4110f
Danny <dongyunrhee@gmail.com>
2019-05-17 03:46:36 +0000

    Added review model/table, model validation.

---
+++ app/models/review.rb
@@ -0,0 +1,8 @@
+class Review < ActiveRecord::Base
+    validates :product_id, presence: true
+    validates :user_id, presence: true
+    validates :description, presence: true
+    validates :rating, presence: true, numericality : { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5}
+
+    belongs_to :product
+end
