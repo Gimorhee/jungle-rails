a99799b8aefa9bf8436b6a37851d23d3e45db3e2
Danny <dongyunrhee@gmail.com>
2019-05-21 20:27:54 +0000

    Finished feature spec: Product details.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,7 @@ ENV['RAILS_ENV'] ||= 'test'
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 # Add additional requires below this line. Rails is not loaded until this point!
 Capybara.javascript_driver = :poltergeist
+Capybara.default_max_wait_time = 10
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
